211be27e7a9456dd333451aa33617cff48d67184
Sascha Depold <sascha@depold.com>
2011-10-10 16:33:21 +0200

    added highlighting

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -15,33 +15,41 @@ Dashboard.Chart = function(options) {
     this.options.width,
     this.options.height
   )
-}
 
-Dashboard.Chart.prototype.rgb = function(offset) {
-  var rgb = this.options.rgb
-    , add = function(a) { return (a+offset > 255) ? 255 : a+offset }
-
-  offset = offset || 0
-
-  return [add(rgb.red), add(rgb.green), add(rgb.blue)].join(',')
+  this.path = null
+  this.highlights = []
 }
 
 Dashboard.Chart.prototype.render = function(values) {
   var path  = this._generatePathString(this.values = values)
     , self  = this
 
-  this
+  this.path = this
     .paper
     .path(path)
-    .attr({stroke: 'rgba(' + this.rgb() + ',1)', fill: 'rgba(' + this.rgb() + ',0.6)', 'stroke-width': 2})
-    .mousemove(function(a,b,c) {
-      self._drawLabel('heyho', 100, 50)
+    .attr({stroke: 'rgba(' + this._rgb() + ',1)', fill: 'rgba(' + this._rgb() + ',0.6)', 'stroke-width': 2})
+    .mousemove(function(e) {
+      self._highlightValue(e)
     })
 
   this._drawAxisLabels(7)
 }
 
 // private
+
+Dashboard.Chart.prototype._rgb = function(offset) {
+  var rgb = this.options.rgb
+    , add = function(a) {
+      var result = a + offset
+      if(result > 255) result = 255
+      if(result < 0)   result = 0
+      return result
+    }
+
+  offset = offset || 0
+
+  return [add(rgb.red), add(rgb.green), add(rgb.blue)].join(',')
+}
 
 Dashboard.Chart.prototype.__defineGetter__('maxValue', function() {
   return this
@@ -98,6 +106,37 @@ Dashboard.Chart.prototype._drawAxisLabels = function(labelCount) {
 
   values.forEach(function(value) {
     var text = self.paper.text(20, self._valueToRelative(value), parseInt(value).toString())
-    self.paper.set().push(text.attr({'font-weight':'bold', 'fill': 'rgba(' + self.rgb(50) + ',1)'}))
+    self.paper.set().push(text.attr({'font-weight':'bold', 'fill': 'rgba(' + self._rgb(50) + ',1)'}))
   })
+}
+
+Dashboard.Chart.prototype._highlightValue = function(e) {
+  var mouseX   = e.offsetX
+    , mouseY   = e.offsetY
+    , segment  = null
+    , linePath = null
+    , attrs    = {stroke: 'rgba(' + this._rgb() + ',1)', 'stroke-width': 2}
+
+  segment = this._generatePathString().split('L').reduce(function(prev, seg) {
+    var x = parseFloat(seg.split(',')[0])
+      , y = parseFloat(seg.split(',')[1])
+
+    if((!prev.x && !prev.y) &&(x >= mouseX) && (x > prev.x))
+      return {x: x, y: y}
+    else
+      return prev
+  }, {x: 0, y: 0})
+
+  linePath = 'M%{x},%{y}L%{x},%{height}'
+  linePath = linePath.replace(new RegExp('%{x}', 'g'), segment.x)
+  linePath = linePath.replace('%{height}', this.options.height)
+  linePath = linePath.replace('%{y}', segment.y)
+
+  var line   = this.paper.path(linePath).attr(attrs)
+    , circle = this.paper.circle(segment.x, segment.y, 6)
+                         .attr(jQuery.extend(attrs, { fill: 'rgba(' + this._rgb() + ',1)' }))
+
+  this.highlights.forEach(function(highlight) { highlight.remove() })
+  this.highlights.push(line)
+  this.highlights.push(circle)
 }
